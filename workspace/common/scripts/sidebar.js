aead95d6dac19876e67ec0ee1485b67cc34d1492
TAEKnical <ilikebigmac@naver.com>
2019-11-20 03:15:23 +0900

    겹침 수정완료

--- workspace/common/scripts/sidebar.js
+++ workspace/common/scripts/sidebar.js
@@ -16,16 +16,29 @@ $(function(){
 
 $(function(){
     var duration = 300;
-
+    var windowWidth = $( window ).width();
     var $side = $('#sidebar');
+    var background = document.getElementById("background");
     var $sidebt = $side.find('button').on('click', function(){
         $side.toggleClass('open');
 
         if($side.hasClass('open')) {
             $side.stop(true).animate({left:'0px'}, duration);
+            if(window.innerWidth < 1000){
+                    background.style.display = 'none';
+                }
         }else{
             $side.stop(true).animate({left:'-300px'}, duration);
-
+            background.style.display = 'block';
         };
     });
 });
+
+$(function(){
+    $(window).resize( funcThisSize );
+    funcThisSize();
+});
+window.onresize = function(event)
+{
+    document.location.reload(true);
+}
