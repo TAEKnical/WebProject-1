4e37a00eab22e7a832ba4c05e2f103e4e93ee6df
TAEKnical <ilikebigmac@naver.com>
2019-11-20 03:08:32 +0900

    login 폼 구현

--- workspace/common/scripts/sidebar.js
+++ workspace/common/scripts/sidebar.js
@@ -13,3 +13,19 @@ $(function(){
         };
     });
 });
+
+$(function(){
+    var duration = 300;
+
+    var $side = $('#sidebar');
+    var $sidebt = $side.find('button').on('click', function(){
+        $side.toggleClass('open');
+
+        if($side.hasClass('open')) {
+            $side.stop(true).animate({left:'0px'}, duration);
+        }else{
+            $side.stop(true).animate({left:'-300px'}, duration);
+
+        };
+    });
+});
