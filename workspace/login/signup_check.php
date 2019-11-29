9816d6e770b2aefb6d82344f3ec169f777fc1d26
mornadina13 <mornadina13@gmail.com>
2019-11-29 16:23:39 +0900

    로그인 완료

--- workspace/login/signup_check.php
+++ workspace/login/signup_check.php
@@ -8,14 +8,19 @@ include("connect.php");
 	$pw = $_POST["pw"];
 	$name = $_POST["name"];
 	$email = $_POST["email"];
-
 	if($id != null && $pw != null && $name != null && $email != null)
 	{	
 		$sql = "SELECT * FROM info WHERE $id='{$id}'";
-		
 		$query = "INSERT INTO info (id,pw,name,email) VALUES ($id, $pw, $name, $email)";
-		echo "<script>alert('회원가입이 완료되었습니다.');</script>";
-		echo "<script>location.href='./login.php';</script>";
+
+		$query = $query;
+		if($con->query($query)){
+			echo "<script>alert('회원가입이 완료되었습니다.');</script>";
+			echo "<script>location.href='./login.php';</script>";	
+		}else{
+			echo "<script>alert('회원가입 실패.');</script>";
+			echo "<script>location.href='./signup.html';</script>";
+		}
 		} /*else{
 			if(is_int($_POST['id']) && !is_string($_POST['name']))
 			{
@@ -29,8 +34,7 @@ include("connect.php");
 				echo "<script>history.back();</script>"; 
 			}
 		}
-		*/
-	}	
+		*/	
 	else{
 		echo "<script>alert('빈칸없이 입력해주세요.'); history.back();</script>";
 	}
