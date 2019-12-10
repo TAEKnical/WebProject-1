2f4f53befd6a4cbd94fb9cea2f1347ec98e42f31
dwkang707 <dwkang707@gmail.com>
2019-12-10 14:05:50 +0900

    12/10 dongwan

--- workspace/qna/add_question.php
+++ workspace/qna/add_question.php
@@ -1,21 +1,40 @@
 <?php
 	session_start();
+	$user_id = $_SESSION['id'];
 	$content = $_POST["ask-content"];
 	$writer = $_SESSION['name'];
 	date_default_timezone_set('Asia/Seoul');
 	$date = date("Y-m-d H:i:s");
 
-	// DB Server Connection
-	$connection = new PDO("mysql:dbname=qna;host=localhost", "root", "root");
-	
-	// 질문 내용이 null이 아니면 작성자, 내용, 시간을 table에 넣은 후 새로 고침한다. 질문 내용이 null이면 새로 고침만 한다.
-	if ($content != null) {
-		$writer = $connection->quote($writer);
-		$content = $connection->quote($content);
-		$date = $connection->quote($date);
-		$connection->exec("INSERT INTO question (writer, content, created, checked) VALUES ($writer, $content, $date, 0);");
-		header('Location: ./qna.php');
-	} else {
-		header('Location: ./qna.php');
-	}
-?>
+	try {
+		// DB Server Connection
+		$connection = new PDO("mysql:dbname=qna;host=localhost", "root", "root");
+		$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		// 질문 내용이 null이 아니면 작성자, 내용, 시간을 table에 넣은 후 새로 고침한다. 질문 내용이 null이면 새로 고침만 한다.
+		if ($user_id != null) {
+			if ($content != null) {
+				$user_id = $connection->quote($user_id);
+				$writer = $connection->quote($writer);
+				$content = $connection->quote($content);
+				$date = $connection->quote($date);
+				$connection->exec("INSERT INTO question (user_id, writer, content, created, checked) VALUES ($user_id, $writer, $content, $date, 0);");
+				header('Location: ./qna.php');
+			} else {
+				header('Location: ./qna.php');
+			}
+		} else { ?>
+			<script type="text/javascript">
+				var chkConfirm = confirm('로그인한 사용자만 질문을 작성하실수 있습니다.\n\n로그인 페이지로 이동할까요?');
+				if (chkConfirm) {
+					location.href='../login/login.php';
+				} else {
+					location.href='./qna.php';
+				}
+			</script>
+		<?php }
+		
+	} catch (PDOException $ex) { ?>
+		<p>Sorry, a database error occurred. Please try again later.</p>
+		<p>(Error details: <?= $ex->getMessage() ?>)</p>
+	<?php } ?>
--- workspace/qna/unchecked.php
+++ workspace/qna/unchecked.php
@@ -1,6 +1,8 @@
 <?php
+	session_start();
 	$created = $_POST["created"];
 	$writer = $_POST["writer"];
+	$user_id = $_SESSION['id'];
 	/* qna.php에서 값 넘어오는거 확인
 	print $created;
 	print $writer; */
@@ -8,11 +10,21 @@
 	//$check = $_POST[""]
 	// DB Server Connection
 	$connection = new PDO("mysql:dbname=qna;host=localhost", "root", "root");
-	
 	$created = $connection->quote($created);
 	$writer = $connection->quote($writer);
-
-	// 답변 완료 버튼을 누를 경우 table에서 checked 칼럼의 값을 1에서 0로 변경
-	$connection->exec("UPDATE question SET checked = 0 WHERE writer = $writer AND created = $created;");
-	header('Location: ./qna.php');
+	$user_id = $connection->quote($user_id);
+	$stmt = $connection->prepare("SELECT admin FROM info WHERE id = $user_id;");
+	$stmt->execute();
+	$admin = $stmt->fetch();
+	
+	// 관리자 권한이 있으면 답변 완료 버튼을 클릭 시 table에서 checked 칼럼의 값을 1에서 0으로 변경
+	// 관리자 권한이 없으면 alert 띄워준 후 qna.php로 되돌아감
+	if ($admin['admin'] == '1') {
+		$stmt = $connection->prepare("UPDATE question SET checked = 0 WHERE writer = $writer AND created = $created;");
+		$stmt->execute();
+		header('Location: ./qna.php');
+	} else {
+		echo "<script>alert('교수님만 사용할 수 있는 기능입니다.');</script>";
+		echo "<script>location.href='./qna.php';</script>";
+	}
 ?>
